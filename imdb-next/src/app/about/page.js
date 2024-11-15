2fad092a3feacdbae512e292e35a815ff151cd5c
primalkarma <primalkarma@gmail.com>
2024-11-15 07:00:33 +0000

    added an about page

---
+++ imdb-next/src/app/about/page.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function () {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Projections based on results coming out of various districts put the
+        final tally for NPP over 150 seats, giving the alliance a rare
+        two-thirds majority in the House, two months after Sri Lankans elected
+        Mr. Dissanayake in a decisive election held after the country’s worst
+        economic crisis since Independence.
+      </p>
+      <p>
+        The NPP increased its vote share across the island but secured an
+        especially remarkable mandate in the north and east that is home to
+        Tamils and Muslims. The poll results in this region overturns the image
+        of the NPP’s chief constituent Janatha Vimukthi Peramuna [JVP or
+        People’s Liberation Front] as an “anti-Tamil rights” party, based on the
+        JVP’s fierce opposition in the 1980s to Tamils’ self-rule and the merger
+        of the north and east.
+      </p>
+      <p>
+        Tamils and Muslims who voted for Mr. Dissanayake’s political rivals —
+        mainly former Opposition Leader Sajith Premadasa — in the presidential
+        election appear to have voted for the NPP, revealing its success in
+        reaching out to ethnic minorities more recently. In almost all
+        districts, barring Batticaloa in the east, the NPP beat prominent
+        regional parties, representing Tamils and Muslims, signalling that the
+        chant for change that dominated the Sinhala-majority south ahead of the
+        September presidential polls had now travelled island-wide.
+      </p>
+    </div>
+  );
+}
